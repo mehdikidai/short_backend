c8e3b2b2b960d44a42acc77859a778abad4fb1e0
mehdikidai <mehdikidai@gmail.com>
2024-11-10 04:13:40 +0100

    edit file python

--- heroku_config.py
+++ heroku_config.py
@@ -1,7 +1,9 @@
 import subprocess
+import os
 
 with open('.env') as f:
     for line in f:
         line = line.strip()
         if line and not line.startswith('#'):
-            subprocess.run(f"heroku config:set {line}", shell=True)
+            #subprocess.run(f"heroku config:set {line}", shell=True)
+            os.system(f"echo {line}")
